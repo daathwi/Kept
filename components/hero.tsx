c507f4d69d461db967ad5e171e0c8dedbd08fcc4
Daathwi <daathwi@Daathwis-MacBook-Air.local>
2026-08-06 11:16:56 +0530

    minor changes

--- components/hero.tsx
+++ components/hero.tsx
@@ -37,7 +37,11 @@ export function Hero() {
               Ticket No. {hero.ticketNo} , {hero.status}
             </span>
           </div>
-          <h1>{hero.title}</h1>
+          <h1>
+            {hero.title.before}
+            <em>{hero.title.emphasis}</em>
+            {hero.title.after}
+          </h1>
           <p className="lede">{hero.lede}</p>
           <div className="hero-actions">
             <a
